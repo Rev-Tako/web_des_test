fd300602b612d4fcf7e99bcd84fe28f9bd8c9209
RevTako <turo15@icloud.com>
2023-06-29 17:07:29 +0100

    none

--- src/lambda/text_input.js
+++ src/lambda/text_input.js
@@ -42,8 +42,8 @@ class ActionProvider {
             const saved = await axios.get(
                 'https://devweb2022.cis.strath.ac.uk/pqb20197-nodejs/save')
             console.log(await saved)
-            console.log(await saved.data.body.SCARLET_output)
-            const output = this.createChatBotMessage(await saved.data.body.SCARLET_output);
+            console.log(await saved.data.body.SCARLET_output.text)
+            const output = this.createChatBotMessage(await saved.data.body.SCARLET_output[0].text);
             this.addMessageToState(output);
         } catch (err) {
             console.log('textSave CATCH')
